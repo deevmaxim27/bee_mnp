e692a244e9f7055bf02adea6b294c6ea01c63dd9
deevmaxim27 <deevmaxim27@gmail.com>
2024-07-20 13:07:40 +0300

    creating project structure

--- pages/index_page.py
+++ pages/index_page.py
@@ -3,6 +3,10 @@ import config
 
 
 class IndexPage:
+    _BUTTON_NACHAT_PERECHOD = "/html/body/div[1]/div[2]/main/div/div[1]/div[2]/div/div/div[2]/div[2]/form/div/div[2]/button"
 
     def open_index_page(self, page: Page) -> None:
         page.goto(config.url.DOMAIN)
+
+    def get_text_from_nachat_perechod_button(self, page: Page) -> None:
+        return page.locator(self, _BUTTON_NACHAT_PERECHOD).get_attribute('class')
--- tests/test_index_page.py
+++ tests/test_index_page.py
@@ -1,10 +1,13 @@
 import pytest
 import pages
-import time
+#import time
 
 
 class TestFooter:
 
     def test_user_should_be_able_to_open_popup_select_subscription_plan(self, page):
         pages.index_page.open_index_page(page)
-        time.sleep(10)
+#        time.sleep(10)
+        actual_result = pages.index_page.get_text_from_nachat_perechod_button(page)
+        assert actual_result == 'Начать Переход', 'Название кнопки не корректно'
+
