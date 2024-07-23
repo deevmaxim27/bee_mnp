a9e9f56fb95949928cbe529dd3ceb0c14a85e066
deevmaxim27 <deevmaxim27@gmail.com>
2024-07-23 10:36:01 +0300

    creating project structure

--- tests/test_index_page.py
+++ tests/test_index_page.py
@@ -9,5 +9,7 @@ class TestFooter:
         pages.index_page.open_index_page(page)
 #        time.sleep(10)
         actual_result = pages.index_page.get_text_from_nachat_perechod_button(page)
-        assert actual_result == 'Начать Переход', 'Название кнопки не корректно'
+        assert actual_result == 'Переход в билайн со своим номером', 'Название кнопки не корректно'
+
+
 
